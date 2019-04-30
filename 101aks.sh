49e136bf4911a6bc2b596a57ab83427df86c9b82
Pieter de Bruin <pieterd@azuresubdocker.onmicrosoft.com>
2019-04-30 15:57:02 +0200

    md to sh

---
+++ 101aks.sh
@@ -0,0 +1,27 @@
+# https://github.com/MicrosoftDocs/azure-docs/blob/master/articles/aks/kubernetes-walkthrough.md
+
+#az account list
+#az account set --subscription
+
+az group create --name rgaks --location westeurope
+
+az aks create --resource-group rgaks --name myAKSCluster --node-count 1 --enable-addons monitoring --generate-ssh-keys --kubernetes-version 1.12.5 --node-vm-size Standard_D2s_v3
+
+az aks enable-addons --resource-group akwrg --name myaks --addons http_application_routing
+
+az aks show --resource-group akwrg --name myaks --query addonProfiles.httpApplicationRouting.config.HTTPApplicationRoutingZoneName -o table
+
+
+az aks get-credentials --resource-group rgaks --name myAKSCluster
+#az aks get-credentials -g=resourcegroupname -n=clustername -f=- >kube.config
+
+#az aks install-cli
+kubectl get nodes
+
+az aks browse --resource-group rgaks --name myAKSCluster #--listen-port 8002
+#kubectl proxy --port 8002
+#api/v1/namespaces/kube-system/services/http:kubernetes-dashboard:/proxy/
+
+# or hpa
+az aks scale --resource-group akwrg --name myaks --node-count 4
+
---
+++ 102helm.sh
@@ -0,0 +1,10 @@
+# Helm
+
+## https://github.com/MicrosoftDocs/azure-docs/blob/master/articles/aks/kubernetes-helm.md
+kubectl apply -f helm-rbac.yaml
+
+helm init --service-account tiller
+
+helm search
+
+helm repo update
