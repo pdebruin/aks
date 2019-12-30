55b97acf8f50d330f4605e7fd0719e56f93ea6d9
Pieter de Bruin <pdebruin@hotmail.com>
2019-12-30 10:00:55 +0100

    update

--- 101aks.sh
+++ 101aks.sh
@@ -11,8 +11,9 @@ az group create --name $resourcegroupname --location $region
 #create aks
 #az aks create --resource-group $resourcegroupname --name $aksname --vm-set-type VirtualMachineScaleSets --node-count 2 --generate-ssh-keys --kubernetes-version 1.14.8 --node-vm-size Standard_B2ms --load-balancer-sku standard
 
-az aks create --resource-group $resourcegroupname --name $aksname --node-count 2 --generate-ssh-keys --kubernetes-version $kubernetesversion --node-vm-size Standard_B2ms --enable-addons monitoring,virtual-node,azure-policy
+az aks create --resource-group $resourcegroupname --name $aksname --node-count 2 --generate-ssh-keys --kubernetes-version $kubernetesversion --node-vm-size Standard_B2ms --enable-addons monitoring,azure-policy
 
+#virtual-node,
 #SLB V
 #VMSS V
 #MNP V
