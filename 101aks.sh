1297e6e620c63eb3d92ea84b187a3b3aec949897
Pieter de Bruin <pdebruin@users.noreply.github.com>
2020-01-03 11:31:06 +0100

    Update 101aks.sh

--- 101aks.sh
+++ 101aks.sh
@@ -9,11 +9,12 @@ export kubernetesversion=1.15.5
 az group create --name $resourcegroupname --location $region
 
 #create aks
-#az aks create --resource-group $resourcegroupname --name $aksname --node-count 2 --generate-ssh-keys --kubernetes-version $kubernetesversion --node-vm-size Standard_B2ms --enable-addons monitoring,azure-policy
+az aks create --resource-group $resourcegroupname --name $aksname --node-count 2 --generate-ssh-keys --kubernetes-version $kubernetesversion --node-vm-size Standard_B2ms --enable-addons monitoring,azure-policy
 
-az aks create --resource-group $resourcegroupname --name $aksname --node-count 3 --zones 1 2 3 --generate-ssh-keys --kubernetes-version $kubernetesversion --node-vm-size Standard_B2ms --enable-addons monitoring,azure-policy
+#az aks create --resource-group $resourcegroupname --name $aksname --node-count 3 --zones 1 2 3 --generate-ssh-keys --kubernetes-version $kubernetesversion --node-vm-size Standard_B2ms --enable-addons monitoring,azure-policy
 
-#virtual-node,
+#av zones
+#virtual-node
 #SLB V
 #VMSS V
 #MNP V
