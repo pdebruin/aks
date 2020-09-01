674f5a0da652d66f397158da27b4f94f31ae1551
Pieter de Bruin <pdebruin@hotmail.com>
2020-09-01 10:53:20 +0200

    20.09 update

--- 101aks.sh
+++ 101aks.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-export resourcegroupname=aksrg202001
 export region=westeurope
-export aksname=aks202001
-export kubernetesversion=1.15.5
+export resourcegroupname=aksrg2009
+export aksname=aks2009
+#az aks get-versions --location $region --output table
+export kubernetesversion=1.17.9
+#az vm list-skus --location $region
+export vmsize=Standard_B2ms
 
 #create a resource group
 az group create --name $resourcegroupname --location $region
 
 #create aks
-az aks create --resource-group $resourcegroupname --name $aksname --node-count 2 --generate-ssh-keys --kubernetes-version $kubernetesversion --node-vm-size Standard_B2ms --enable-addons monitoring,azure-policy
-
-#az aks get-versions --location westeurope --output table
-
-#az vm list-skus --location westeurope
+az aks create --resource-group $resourcegroupname --name $aksname --node-count 2 --generate-ssh-keys --kubernetes-version $kubernetesversion --node-vm-size $vmsize 
 
 #az feature list -o table --query "[?contains(name, 'Microsoft.ContainerService')].{Name:name,State:properties.state}"
