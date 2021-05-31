3565e5edf2dfd7d4d41fcca43606015c4885e08a
Microsoft Learn Student <learn@contoso.com>
2021-05-31 14:40:31 +0200

    2106

--- 101aks.sh
+++ 101aks.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
+export resourcegroupname=aks2106rg
+export aksname=aks2106
 export region=westeurope
-export resourcegroupname=aksrg2009
-export aksname=aks2009
 #az aks get-versions --location $region --output table
-export kubernetesversion=1.17.9
+export kubernetesversion=1.20.5
 #az vm list-skus --location $region
 export vmsize=Standard_B2ms
 
 #create a resource group
 az group create --name $resourcegroupname --location $region
 
-#create aks
-az aks create --resource-group $resourcegroupname --name $aksname --node-count 2 --generate-ssh-keys --kubernetes-version $kubernetesversion --node-vm-size $vmsize 
-
 #az feature list -o table --query "[?contains(name, 'Microsoft.ContainerService')].{Name:name,State:properties.state}"
+
+#create aks
+az aks create --resource-group $resourcegroupname --name $aksname --node-count 2 --generate-ssh-keys --kubernetes-version $kubernetesversion --node-vm-size $vmsize
