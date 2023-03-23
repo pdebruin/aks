095eef3bece8e8a419c6457116328c4a7ac25657
pwje <pieterd@deskhex>
2023-03-23 11:12:25 +0100

    aks version update

--- 101aks.sh
+++ 101aks.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
+# curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 
 export resourcegroupname=aks2303rg
 export aksname=aks2303
 export region=westeurope
 #az aks get-versions --location $region --output table
-export kubernetesversion=1.20.5
-#az vm list-skus --location $region
+export kubernetesversion=1.25.5
+#az vm list-skus --location $region --output table
 export vmsize=Standard_B2ms
+export admingroupid=2103f131-9cbf-4f9f-8290-0f02ad527838
 
 #create a resource group
 az group create --name $resourcegroupname --location $region
@@ -14,4 +16,19 @@ az group create --name $resourcegroupname --location $region
 #az feature list -o table --query "[?contains(name, 'Microsoft.ContainerService')].{Name:name,State:properties.state}"
 
 #create aks
-az aks create --resource-group $resourcegroupname --name $aksname --node-count 2 --generate-ssh-keys --kubernetes-version $kubernetesversion --node-vm-size $vmsize
+az aks create --resource-group $resourcegroupname --name $aksname --node-count 2 --generate-ssh-keys --kubernetes-version $kubernetesversion --node-vm-size $vmsize --enable-aad --aad-admin-group-object-ids $admingroupid
+
+#-monitoring
+#--enable-managed-identity
+#az aks update -g $resourcegroupname -n $aksname --enable-aad --aad-admin-group-object-ids $admingroupid
+#az aks update -g myResourceGroup -n myAKSCluster --enable-azure-rbac
+
+
+#az aks install-cli # to install kubectl to interact with the cluster
+#https://kubernetes.io/docs/tasks/tools/install-kubectl-linux/
+
+az aks get-credentials --resource-group $resourcegroupname --name $aksname
+
+kubectl version
+
+kubectl get nodes
